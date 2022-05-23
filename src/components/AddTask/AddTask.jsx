88d3c9f4b551aa5ea58bf1d0653ac94b1e85c3b9
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-23 14:30:03 +0530

    update

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = () => {
+const AddTask = ({todos,setTodos,inputText,setInputText}) => {
   // NOTE: do not delete `data-cy` key value pair
+  const inputHandler=(e)=>{
+    // console.log(e.target.value);
+    setInputText(e.target.value);
+  }
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+      <input data-cy="add-task-input" type="text" value={inputText} onChange={inputHandler}/>
+      <button data-cy="add-task-button">+</button>
     </div>
   );
 };
--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./taskApp.module.css";
+import tasks from "../data/tasks.json"
+import AddTask from "./AddTask/AddTask";
 
 const TaskApp = () => {
+
+  const [todos,setTodos]=useState(tasks);
+  const [inputText,setInputText]=useState("");
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div data-cy="task-app" className={styles.taskApp}>
       {/* Header */}
+      <h1>Todo List </h1>
       {/* Add Task */}
+      <AddTask inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}/>
       {/* Tasks */}
     </div>
   );
